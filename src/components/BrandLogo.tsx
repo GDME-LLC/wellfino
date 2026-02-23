3e376377efbfbf0ebd77727a64b29ddd80a36b20
gabrial <gabrial@gdmellccom.com>
2026-02-22 23:50:56 -0500

    logo update 3

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import wellfinoIcon from "@/assets/wellfino-icon.svg";
 
 type BrandLogoProps = {
   className?: string;
@@ -9,7 +10,7 @@ type BrandLogoProps = {
 const BrandLogo = ({ className, iconClassName, textClassName }: BrandLogoProps) => {
   return (
     <span className={cn("inline-flex items-center gap-2", className)}>
-      <img src="/favicon.png" alt="Wellfino icon" className={cn("h-9 w-9 object-contain", iconClassName)} />
+      <img src={wellfinoIcon} alt="Wellfino icon" className={cn("h-9 w-9 object-contain", iconClassName)} />
       <span
         className={cn("text-5xl font-semibold leading-none tracking-tight text-foreground", textClassName)}
         style={{ fontFamily: "'Playfair Display', serif" }}
